d2af876a7315b57271d0556b5a6ee8266aa5207c
Davi Martins <davi15mmartins@gmail.com>
2026-09-11 16:46:29 -0300

    adiciona nova const ao diagnostico.js

--- aula01-revisao-nodejs-npm/diagnostico.js
+++ aula01-revisao-nodejs-npm/diagnostico.js
@@ -1,12 +1,13 @@
 //! importa o modulo 'os' do Node.js para obter informações sobre o servidor
 const os = require('os');
 
+
 //! exibe informações do sistema opereacional e hardware do servidor
 console.log('===DIAGNÓTIGO DO SERVIDOR  ===\n');
 
 const plataforma = os.platform();
-const memoriaTotal = (os.totalmem() / (1024 ** 3)).toFixed(2);
-const memoriaLivre = (os.freemem() / (1024 ** 3)).toFixed(2);
+const memoriaTotal = (os.totalmem() / (1024 ** 3).toFixed(2));
+const memoriaLivre = (os.freemem() / (1024 ** 3).toFixed(2));
 const cpus = os.cpus();
 
 console.log(`Arquitetura OS: ${plataforma}`);
