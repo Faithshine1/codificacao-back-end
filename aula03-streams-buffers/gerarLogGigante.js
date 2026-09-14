d2fdc48ee497a177aa15b2dfd8af3314ac302d3a
Davi Martins <davi15mmartins@gmail.com>
2026-09-14 17:10:21 -0300

    feat(gerarLogsGigante.js): adiciona gerador de logs

--- aula03-streams-buffers/gerarLogGigante.js
+++ aula03-streams-buffers/gerarLogGigante.js
@@ -2,3 +2,9 @@ import fs from 'fs';
 
 const streamEscrita = fs .createWriteStream('Servidor.log');
 console.log('Gerando Arquivo de log simulado...');
+
+for(let i = 0; i < 200000; i++){
+    const tipo = i % 7 === 0 ? 'ERROR' : 'INFO';
+    streamEscrita.write(`[2026-09-11] Line ${i}: Status 200 - mensagem de teste ${tipo}\n`);
+}
+streamEscrita.end();
